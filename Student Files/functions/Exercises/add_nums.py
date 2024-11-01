e063c714940fbcc60629e06f07e692fc45499b67
Lucas Matignon <Lucas.c.matignon@gmail.comit config --global user.emailit config --global user.email lucas.c.matignon@gmail.com>
2024-11-01 15:03:21 +0100

    added student files, excercise 6 done

---
+++ Student Files/functions/Exercises/add_nums.py
@@ -0,0 +1,32 @@
+# def add_3_and_6():
+#     total = 3 + 6
+#     print('3 + 6 = ', total)
+
+# def add_10_and_12():
+#     total = 10 + 12
+#     print('10 + 12 = ', total)
+
+# def main():
+#     add_3_and_6()
+#     add_10_and_12()
+    
+def add_nums(num1, num2):
+    total = num1 + num2 
+    print(num1, '+', num2, '=', total)
+
+def main():
+    add_nums(3,6)
+    add_nums(10,12)
+
+main()   
+
+
+def add_nums(num1, num2):
+    total = num1 + num2
+    print(num1, '+', num2, '=', total)
+
+def main():
+    add_nums(4,5)
+    add_nums(34,12)
+
+main()
