d440541d9205bec37588113c0bbb7957e066e491
Lucas Matignon <Lucas.c.matignon@gmail.comit config --global user.emailit config --global user.email lucas.c.matignon@gmail.com>
2024-11-01 14:08:15 +0100

    Functions examples

--- hello_you.py
+++ hello_you.py
@@ -1,5 +1,8 @@
+def say_hello():
+    your_name = input("What is your name? ")
+    print("Hello, ", your_name, "!", sep="")
+
 def main():
-    your_name = input("What is your name?")
-print ('')
+    say_hello()
 
 main()
